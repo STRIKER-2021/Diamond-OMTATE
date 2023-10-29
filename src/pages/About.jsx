831ac9dca9011693d847428b0d087720f654e8bc
OM TATE <dcomtate1234@gmail.com>
2023-10-29 12:09:51 +0530

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './About.css';
+
+export const About = () => {
+  return (
+    <div className='about'>
+    <div className="container">
+    <div className="image"></div>
+    <div className="text-box">
+      <p>Your text </p>
+    </div>
+  </div>
+
+  <div className="container-2">
+    
+    <div className="text-box">
+      <p>Your text </p>
+    </div>
+    <div className="image"></div>
+  </div>
+  <div className="container">
+    <div className="image"></div>
+    <div className="text-box">
+      <p>Your text </p>
+    </div>
+  </div>  
+  </div>
+ )
+}
